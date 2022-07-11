ee0987c0b2d26679ce04494c7dcb876df1369114
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2022-07-11 12:58:08 +0530

    fix touch issues

--- src/components/menu-list-item/menu-list-item.model.ts
+++ src/components/menu-list-item/menu-list-item.model.ts
@@ -5,6 +5,10 @@ import { MenuItemProps } from "../menu/menu-model";
 export type MenuItemViewModel = MenuItemProps & {
   icon?: ReactNode;
   open?: boolean | null;
-  onSelect?: (path: string, index: number) => void;
+  onSelect?: (path: string, index: number, id?: string) => void;
   index: number;
+  onCloseSubMenu: () => void;
+  onMouseEnter: (id?: string) => void;
+  onMouseLeave: (id?: string) => void;
+  onToggleSubMenu: (id?: string) => void;
 } & Pick<MenuHeadProps, "iconSize">;
